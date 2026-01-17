0272352f18179c3b4af7ba4a57c345511a31688f
Yves-Leon <leonkouayep@gmail.com>
2026-01-17 17:48:25 +0100

    tailwindconfig erweitert

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        float: {
+          '0%,100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-6px)' },
+        },
+      },
+      animation: {
+        float: 'float 6s ease-in-out infinite',
+      },
+    },
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-  ],
+  plugins: [require('@tailwindcss/forms')],
 }
+
